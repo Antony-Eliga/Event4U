4802d6feb999e81c474cd0b7fd41ddbba90ea5a7
Antony Vitré <antony@eliga.fr>
2018-05-14 12:22:04 +0200

    bonjour

---
+++ Event4U/Event4U/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Event4U.Startup))]
+namespace Event4U
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
