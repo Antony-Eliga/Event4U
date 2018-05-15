a0974bbe1b198e24d2bb8886d961d0a704250d8b
Antony Vitré <antony@eliga.fr>
2018-05-15 10:42:03 +0200

    Ajout Service Parking

--- Event4U/Event4U/Controllers/HomeController.cs
+++ Event4U/Event4U/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using BO;
+using Event4U.Services;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,7 +14,12 @@ namespace Event4U.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            string id = "colombier";
+            Parks.Feature feature = ParkService.getOneFeature(id);
+            Parks.Park park = ParkService.getOnePark(id);
+            Console.WriteLine(park.parkInformation.name);
+            Console.WriteLine(feature.geometry.coordinates[0] + " " + feature.geometry.coordinates[1]);
+            return View(ParkService.getAllParks());
         }
 
         public ActionResult About()
