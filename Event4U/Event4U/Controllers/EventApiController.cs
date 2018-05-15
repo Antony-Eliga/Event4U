399b16636835af8fb5983876a44eaf4f1c7c4096
LUCAS FABIEN <fabien.lucas2016@campus-eni.fr>
2018-05-15 16:58:22 +0200

    Fin de journée

---
+++ Event4U/Event4U/Controllers/EventApiController.cs
@@ -0,0 +1,23 @@
+﻿using Event4U.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using BO;
+
+namespace Event4U.Controllers
+{
+    public class EventApiController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Events
+        public JsonResult IndexJson()
+        {
+            return Json(db.Events.ToList(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
