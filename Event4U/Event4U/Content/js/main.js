1c8bc98fc3e40bd584535569865cb96c01e0f471
LUCAS FABIEN <fabien.lucas2016@campus-eni.fr>
2018-05-16 13:55:25 +0200

    main.js

---
+++ Event4U/Event4U/Content/js/main.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $(".eventList").html();
+
+    $.getJSON("EventApi/IndexJson", function (data) {
+        $.each(data, function (key, val) {
+            $(".eventList .row").append(eventHtml(val));
+        });
+    });
+
+    function eventHtml(val) {
+        console.log(val.name + val.lat + ' ' + val.lng);
+        var html = "<div class='col s12 m4'><div class='card-panel'>" + val.name + "</div></div>";
+        return html;
+    };
+});
