5af3f525a3d0de1cfbb9bc97abb0b4896d5fc283
LUCAS FABIEN <fabien.lucas2016@campus-eni.fr>
2018-05-16 10:16:33 +0200

    Bonjour

--- Event4U/Event4U/Content/js/main.js
+++ Event4U/Event4U/Content/js/main.js
@@ -9,7 +9,15 @@
         var element = document.getElementById(id);
         if (element) {
             var autocomplete = new google.maps.places.Autocomplete(element, { types: ['geocode'] });
-            google.maps.event.addListener(autocomplete, 'place_changed', functionAAntony);
+            google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
         }
+    }
+
+    function onPlaceChanged() {
+        var place = this.getPlace();
+        
+        //Bouton
+
+        console.log(place);
     }
 });
